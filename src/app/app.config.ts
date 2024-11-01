59e2845dac67434ab34e216d8c23e11f1fbd9381
victorVSilva14 <victorvieira6054@gmail.com>
2024-10-31 21:04:14 -0300

    feature: adding navbar to default layout

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection(),
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()), provideAnimationsAsync()
   ]
 };
--- src/app/components/default-home-layout/default-home-layout.component.ts
+++ src/app/components/default-home-layout/default-home-layout.component.ts
@@ -1,13 +1,35 @@
 import { Component, Input } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { BudgetTableComponent } from '../budget-table/budget-table.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-default-home-layout',
   standalone: true,
   imports: [
+    MatTableModule,
+    MatPaginatorModule,
+    BudgetTableComponent,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   templateUrl: './default-home-layout.component.html',
   styleUrl: './default-home-layout.component.scss'
 })
 export class DefaultHomeLayoutComponent {
   @Input() title: string = "";
+  isMobile: boolean = false;
+
+  constructor() {
+    this.checkScreenSize();
+    window.addEventListener('resize', () => this.checkScreenSize());
+}
+
+checkScreenSize() {
+    this.isMobile = window.innerWidth <= 768; 
+}
 }
